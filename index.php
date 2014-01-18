8f5f9816971e9ca9a151129985bb4edcf129d8fc
Floris Jansen <florismartijnjansen@gmail.com>
2014-01-18 14:09:57 +0100

    v2013

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <?php
   }
 ?>
-      <p>Hi, I'm Floris. I live in The Netherlands, near Amsterdam. While listening to all kinds of music and drinking tea, I design websites, logos and other graphical stuff. Please take a look at what I've made at my <a href="/portfolio">portfolio</a> and feel free to <a href="mailto:floris@fmjansen.nl">email</a> me if you want to. I'm also on <a href="http://twitter.com/fmjansen" id="twitter">Twitter</a>, <a href="http://dribbble.com/fmjansen" id="dribbble">Dribbble</a> and <a href="http://instagram.com/fmjansennl" id="instagram">Instagram</a>.</p>
+      <p>Hi, I'm Floris. I live in The Netherlands, near Amsterdam. While listening to all kinds of music and drinking tea, I design websites, logos and other graphical stuff. Please take a look at what I've made at my <a href="/portfolio">portfolio</a> and feel free to <a href="mailto:floris@fmjansen.nl">email</a> me if you want to. I'm also on <a href="http://twitter.com/fmjansen" id="twitter">Twitter</a> and <a href="http://dribbble.com/fmjansen" id="dribbble">Dribbble</a>.</p>
 
 <?php
   if($_GET['ajax'] !== '1') {
