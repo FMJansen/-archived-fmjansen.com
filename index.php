0b12a057ae17c96d1ddb665a2364f6d3f034f106
Floris Jansen <florismartijnjansen@gmail.com>
2014-01-19 01:08:49 +0100

    first try at mini-portfolio

--- index.php
+++ index.php
@@ -6,7 +6,15 @@
   <p>Hi, I'm Floris. I live in The Netherlands, near Amsterdam. I design websites, logos and other graphical stuff. I'm working on <a href="http://www.freevle.org">Freevle</a>, an open source CMS for schools, which could eventually become a <span title="Virtual learning enviroment">VLE</span> aswell. Furthermore, I've built <a href="http://careljansen.com">CarelJansen.com</a>, <a href="http://willemdeblaauw.com">WillemdeBlaauw.com</a> and <a href="http://scoutingoost1.nl">Scoutingoost1.nl</a> and designed a <a href="http://www.informatics-europe.org/images/documents/more-women-in-informatics-research-and-education_2013.pdf">booklet</a> for <a href="http://www.informatics-europe.org/services/women-in-icst-research-and-education.html">Informatics Europe</a>, our <a href="/cygnus-x1">school paper</a>, posters and a booklet for staging of <a href="/andromache">Andromache</a> and <a href="/hamlet">Hamlet</a>. </p>
 
   <p>Feel free to <a href="mailto:floris@fmjansen.nl">email</a> me if you want to. I'm also on <a href="http://twitter.com/FMJansen">Twitter</a>, <a href="http://fmjansen.tumblr.com">Tumblr</a>, <a href="http://kippt.com/fmjansen">Kippt</a>, <a href="http://github.com/FMJansen">Github</a> and <a href="http://dribbble.com/FMJansen">Dribbble</a>.</p>
-    </div>
+</div>
+
+<div id="showoff">
+  <div class="close">&raquo;</div>
+  <h2>Hamlet</h2>
+  <div id="container">
+    <img src="/static/img/hamlet.png">
+  </div>
+</div>
 
 <?php
   include('footer.php');
