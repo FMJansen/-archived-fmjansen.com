68602f8aca762d361318f2e476701effc142eaa0
Floris Jansen <florismartijnjansen@gmail.com>
2014-01-19 01:09:01 +0100

    edit 404-text

--- 404.php
+++ 404.php
@@ -1,18 +1,15 @@
 <?php
   $title = '404 Not found | ';
-  $bodyclass = ' class="notfound"';
   include('header.php');
 ?>
-    <div id="content">
-      <h2>Meh :/</h2>
-      <h3>404 Page not found</h3>
 
-      <p>Pitty, the server couldn't find this page, so you got to see this boring 404-page. Please use the navigation above to start browsing my site again. Are you sure this page should exist? Please <a href="mailto:floris@fmjansen.nl">contact me</a>, thanks!</p>
-    </div>
+<div id="content">
+  <h2>Meh :/</h2>
+  <h3>404 Page not found</h3>
 
-    <footer>
-      <p>FMJansen.nl - Floris Jansen</p>
-    </footer>
+  <p>Pitty, the server couldn't find this page, so you got to see this boring 404-page. You'll probably want to head <a href="/">home</a>.</p>
+</div>
 
-  </body>
-</html>
+<?php
+  include('footer.php');
+?>
