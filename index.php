e9e040d4642bf1885309e3b09a55c21d72572085
Floris Jansen <floris@fmjansen.nl>
2014-05-22 22:47:47 +0200

    add last.fm

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div id="content">
   <p>Hi, I'm Floris. I live in The Netherlands, near Amsterdam. I design websites, posters and more graphical work for paper aswell as the web. I'm working on <a href="http://www.freevle.org">Freevle</a>, an open source CMS for schools, which could eventually become a <span title="Virtual learning enviroment">VLE</span> aswell. Furthermore, I've built <a href="http://careljansen.com">Carel<wbr>Jansen.com</a>, <a href="http://willemdeblaauw.com">Willem<wbr>de<wbr>Blaauw.com</a> and <a href="http://scoutingoost1.nl">Scoutingoost1.nl</a> and designed a <a href="http://www.informatics-europe.org/images/documents/more-women-in-informatics-research-and-education_2013.pdf">booklet</a> for <a href="http://www.informatics-europe.org/services/women-in-icst-research-and-education.html">Informatics Europe</a>, our <a href="cygnus-x1">school paper</a> and posters and a booklet for the staging of <a href="andromache">Andromache</a> and <a href="hamlet">Hamlet</a>. </p>
 
-  <p>Feel free to <a href="mailto:floris@fmjansen.nl">email</a> me if you want to. I'm also on <a href="http://twitter.com/FMJansen">Twitter</a>, <a href="http://fmjansen.tumblr.com">Tumblr</a>, <a href="http://kippt.com/fmjansen">Kippt</a>, <a href="http://github.com/FMJansen">Github</a> and <a href="http://dribbble.com/FMJansen">Dribbble</a>.</p>
+  <p>Feel free to <a href="mailto:floris@fmjansen.nl">email</a> me if you want to. I'm also on <a href="http://twitter.com/FMJansen">Twitter</a>, <a href="http://fmjansen.tumblr.com">Tumblr</a>, <a href="http://kippt.com/fmjansen">Kippt</a>, <a href="http://github.com/FMJansen">Github</a>, <a href="http://www.last.fm/user/lastfmjansen">Last.fm</a> and <a href="http://dribbble.com/FMJansen">Dribbble</a>.</p>
 </div>
 
 <?php
