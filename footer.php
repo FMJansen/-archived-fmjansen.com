581405964d545a6db5a11151d15145dc8c74941d
Floris Jansen <florismartijnjansen@gmail.com>
2017-07-24 17:23:58 +0200

    remove rubbish work items

--- footer.php
+++ footer.php
@@ -5,41 +5,6 @@
       </blockquote>
     </footer>
 
-    <script type="text/javascript">
-      var workLink = document.querySelector('a[href="/work"]');
-      workLink.addEventListener("click", openWork, false);
-
-      function openWork(event) {
-        // history.pushState(null, null, 'work');
-        event.preventDefault();
-
-        document.body.classList.add('noscroll');
-
-        let backgroundShapes = document.querySelectorAll('.work__part');
-        let index = 0;
-        for( index = 0; index < backgroundShapes.length; index++ ) {
-          backgroundShapes[index].classList.add('open');
-        }
-      }
-
-      document.addEventListener("keyup", closeWork, false);
-
-      function closeWork(event) {
-        if (event.keyCode !== 27) return;
-
-        // history.pushState(null, null, '/');
-        event.preventDefault();
-
-        document.body.classList.remove('noscroll');
-
-        let backgroundShapes = document.querySelectorAll('.work__part');
-        let index = 0;
-        for( index = 0; index < backgroundShapes.length; index++ ) {
-          backgroundShapes[index].classList.remove('open');
-        }
-      }
-    </script>
-
     <!--[if lt IE 9]>
       <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
