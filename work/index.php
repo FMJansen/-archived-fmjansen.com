157eb2570dc42a8055f537ee1cc58c2b75d21ba8
Floris Jansen <florismartijnjansen@gmail.com>
2017-07-24 20:59:35 +0200

    add work page

---
+++ work/index.php
@@ -0,0 +1,235 @@
+<?php $page_title = 'Werk - '; include('../header.php'); ?>
+
+<header>
+  <h1 class="site_title site_title--top">FMJansen</h1>
+</header>
+
+<section class="work">
+
+	<h2 class="work__title">M’n werk</h2>
+
+	<div class="line"></div>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Turn The Page</h3>
+			<time datetime="2015-2016">2015-2016</time>
+		</div>
+		<div class="description">
+			<p>Eindredactie layout van <a href="http://turnthepage.nl/">Turn The Page</a>, het magazine van <a href="//studieverenigingid.nl">Studievereniging i.d</a></p>
+			<img alt="Een cover van Turn The Page"
+				srcset="/static/img/turnthepage_cover.png 1x,
+					/static/img/turnthepage_cover@2x.png 2x"
+				src="/static/img/turnthepage_cover.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Room for Discussion</h3>
+			<time datetime="2016">2016</time>
+		</div>
+		<div class="description">
+			<p>Site van <a href="//roomfordiscussion.com/">Room for Discussion</a>, op basis van een huisstijl bestaande uit een logo en kleuren.</p>
+			<img alt="Screenshot site Room for Discussion"
+				srcset="/static/img/rfd.png 1x,
+					/static/img/rfd@2x.png 2x"
+				src="/static/img/rfd.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Ouderdag IO</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="http://id.tudelft.nl/ouderdag/2015/">Ouderdag IO 2015</a>, inclusief inschrijfsysteem in Python. In samenwerking met <a href="http://emilflach.com/">Emil Flach</a>. Front-end op basis van de site van <a href="http://id.tudelft.nl/ejw/2015/"><abbr title="Eerstejaarsweekend">EJW</abbr> 2015</a>.</p>
+			<img alt="Screenshot site Ouderdag IO 2015"
+				srcset="/static/img/ouderdag.png 1x,
+					/static/img/ouderdag@2x.png 2x"
+				src="/static/img/ouderdag.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Turn The Page website</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="//turnthepage.nl/">Turn The Page</a>, het magazine van <a href="//studieverenigingid.nl">Studievereniging i.d</a></p>
+			<img alt="Screenshot site Turn The Page"
+				srcset="/static/img/turnthepage.png 1x,
+					/static/img/turnthepage@2x.png 2x"
+				src="/static/img/turnthepage.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Scouting Oost 1</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="http://scoutingoost1.nl/">Scouting Oost 1</a></p>
+			<img alt="Screenshot site Scouting Oost 1"
+				srcset="/static/img/scouting.png 1x,
+					/static/img/scouting@2x.png 2x"
+				src="/static/img/scouting.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>EJW</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="http://id.tudelft.nl/ejw/2015/"><abbr title="Eerstejaarsweekend">EJW</abbr> 2015 van <abbr title="Industrieel Ontwerpen">IO</abbr></a>, samen met <a href="http://emilflach.com/">Emil Flach</a></p>
+			<img alt="Screenshot site EJW"
+				srcset="/static/img/ejw.png 1x,
+					/static/img/ejw@2x.png 2x"
+				src="/static/img/ejw.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>IOB</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Inhoud site, <a href="http://iob2014.id.tudelft.nl/sign-up/">aanmeldformulier</a> en promotiemateriaal (stickers, posters, flyers, etc.) <a href="http://iob2014.id.tudelft.nl/"><abbr title="Industrieel Ontwerpen Bedrijvenbeurs">IOB</abbr></a>, samen met <a href="http://emilflach.com/">Emil Flach</a> en <a href="https://linkedin.com/in/michalionarons">Micha Lionarons</a></p>
+			<img alt="Poster voor het Career event van de IOB met het programma"
+				srcset="/static/img/iob.png 1x,
+					/static/img/iob@2x.png 2x"
+				src="/static/img/iob.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Animatie NIOO</h3>
+			<time datetime="2015">2015</time>
+		</div>
+		<div class="description">
+			<p>Animatie voor het <a href="https://nioo.knaw.nl/"><abbr title="Nederlands Instituut voor Ecologie">NIOO</abbr></a>, <a href="https://youtu.be/48yFWouoVFg?t=401">getoond</a> bij de <a href="http://www.universiteitvannederland.nl/">Universiteit van Nederland</a></p>
+			<img alt="Still animatie NIOO"
+				srcset="/static/img/algen-NIOO.png 1x,
+					/static/img/algen-NIOO@2x.png 2x"
+				src="/static/img/algen-NIOO.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Schaverij Utrecht</h3>
+			<time datetime="2014">2014</time>
+		</div>
+		<div class="description">
+			<p>Bouw site <a href="http://www.schaverijutrecht.nl/">Schaverij Utrecht</a> in opdracht van het inmiddels gestopte bureau <a href="http://zeative.nl/">Zeative</a>, ontwerp door Zeative</p>
+			<img alt="Screenshot site Schaverij Utrecht"
+				srcset="/static/img/schaverij.png 1x,
+					/static/img/schaverij@2x.png 2x"
+				src="/static/img/schaverij.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>WIRE</h3>
+			<time datetime="2013">2013</time>
+		</div>
+		<div class="description">
+			<p>Boekje <a href="http://www.informatics-europe.org/images/documents/more-women-in-informatics-research-and-education_2013.pdf">More women in informatics research and education</a> voor <a href="http://www.informatics-europe.org/services/women-in-icst-research-and-education.html">Informatics Europe</a></p>
+			<img alt="Deel cover WIRE booklet"
+				srcset="/static/img/WIRE.png 1x,
+					/static/img/WIRE@2x.png 2x"
+				src="/static/img/WIRE.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Freevle</h3>
+			<time datetime="2013">2013</time>
+		</div>
+		<div class="description">
+			<p>Ontwerp en front-end development voor <a href="http://freevle.org">Freevle</a>, een CMS voor scholen met <a href="https://www.facebook.com/mpvharmelen">Martin van Harmelen</a>, <a href="https://twitter.com/pimtt">Pim ten Thije</a> en <a href="http://martijnarts.com/">Martijn Arts</a> (ontwikkeling onderbroken/gestopt)</p>
+			<img alt="Screenshot demo Freevle voor het Cygnus Gymnasium"
+				srcset="/static/img/freevle.png 1x,
+					/static/img/freevle@2x.png 2x"
+				src="/static/img/freevle.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Cygnus-X1</h3>
+			<time datetime="2013">2013</time>
+		</div>
+		<div class="description">
+			<p>Opmaakredacteur schoolkrant Cygnus-X1 (edities
+				<a href="http://issuu.com/cygnusx-1/docs/x-12">12</a>,
+				<a href="http://issuu.com/cygnusx-1/docs/x-13">13</a> en
+				<a href="http://issuu.com/cygnusx-1/docs/x-14">14</a> met Martin van Harmelen,
+				<a href="http://issuu.com/cygnusx-1/docs/x-15">15</a>,
+				<a href="http://issuu.com/cygnusx-1/docs/x-16">16</a> en
+				<a href="http://issuu.com/cygnusx-1/docs/alles17">17</a> met Samuel Cohn)</p>
+				<img alt="Inhoudsopgave en redactioneel Cygnus X-17"
+					srcset="/static/img/cygnusx1.png 1x,
+						/static/img/cygnusx1@2x.png 2x"
+					src="/static/img/cygnusx1.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Willem de Blaauw</h3>
+			<time datetime="2012">2012</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="http://willemdeblaauw.com">Willem de Blaauw</a></p>
+			<img alt="Screenshot site Willem de Blaauw"
+				srcset="/static/img/willemdeblaauw.png 1x,
+					/static/img/willemdeblaauw@2x.png 2x"
+				src="/static/img/willemdeblaauw.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Carel Jansen</h3>
+			<time datetime="2012">2012</time>
+		</div>
+		<div class="description">
+			<p>Site <a href="http://careljansen.com">Carel Jansen</a></p>
+			<img alt="Screenshot site Carel Jansen"
+				srcset="/static/img/careljansen.png 1x,
+					/static/img/careljansen@2x.png 2x"
+				src="/static/img/careljansen.png">
+		</div>
+	</article>
+
+	<article class="work__item">
+		<div class="date-title">
+			<h3>Promotie toneel</h3>
+			<time datetime="2012">2012</time>
+		</div>
+		<div class="description">
+			<p>Promotie opvoering van Hamlet en later Andromache op school</p>
+			<img alt="Logo Andromache"
+				srcset="/static/img/andromache.png 1x,
+					/static/img/andromache@2x.png 2x"
+				src="/static/img/andromache.png">
+		</div>
+	</article>
+
+	<p class="phone_call__message phone_call__message--center">
+		<span class="site_title">FMJansen</span> [KvK: 64878627, Laatste update: <time datetime="2017-07">juli 2017</time>]
+	</p>
+
+</section>
+
+<?php include('../footer.php'); ?>
